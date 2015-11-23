79990ef47839e2646a3d45da66455130fb84c0a4
Nik Nyby <nnyby@columbia.edu>
2015-11-23 17:43:01 -0500

    add Mediathread.safariextension

---
+++ Mediathread.safariextension/src/global.js
@@ -0,0 +1,9 @@
+var itemArray = safari.extension.toolbarItems;
+
+for (var i = 0; i < itemArray.length; ++i) {
+
+    var item = itemArray[i];
+
+    if (item.identifier == "mediathread") {
+    }
+}
